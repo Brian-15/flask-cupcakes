fc51ffed1f6b3bf3fcdf8b48f18e6cf2e0a73ea6
Brian-15 <bcirieco@gmail.com>
2021-09-07 18:19:00 -0700

    implemented Cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, request, redirect, flash
+from models import Cupcake, connect_db, db
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcakes"
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "secret123"
+
+connect_db(app)
